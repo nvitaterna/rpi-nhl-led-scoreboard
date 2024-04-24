51310921b25bf2b6179688a8554219ffd20b45e8
Nicolas Vitaterna <nvitaterna@gmail.com>
2024-04-24 19:41:48 -0400

    chore: update linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,9 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
 ];
