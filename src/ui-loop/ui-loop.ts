fb2b9de5bede1dda23afc7c252b242aa87c8772c
Nicolas Vitaterna <nvitaterna@gmail.com>
2024-04-24 21:03:34 -0400

    wip

--- src/ui-loop/ui-loop.ts
+++ src/ui-loop/ui-loop.ts
@@ -1,23 +1,34 @@
 import { LiveGameScene } from '@/live-game.scene';
+import { LoggerService } from '@/logger/logger.service';
 import { Loopable } from '@/loopable/loopable';
 import { App } from '@/main';
 import { LedMatrixInstance } from '@nvitaterna/rpi-led-matrix';
+import { Logger } from 'pino';
 
 export class UiLoop extends Loopable {
+  private logger: Logger;
+
+  private liveGameScene: LiveGameScene | null = null;
+
   constructor(
     private app: App,
     private matrix: LedMatrixInstance,
+    loggerService: LoggerService,
   ) {
     super(250);
+    this.logger = loggerService.child('ui-loop');
   }
 
   loop() {
     const uiData = this.app.uiDataService.getFromCache();
     if (!uiData) {
+      this.logger.debug('No UI data found');
       return;
     }
-    const liveGameScene = new LiveGameScene(this.matrix, uiData);
+    if (!this.liveGameScene) {
+      this.liveGameScene = new LiveGameScene(this.matrix, uiData);
+    }
 
-    liveGameScene.render();
+    this.liveGameScene.render();
   }
 }
