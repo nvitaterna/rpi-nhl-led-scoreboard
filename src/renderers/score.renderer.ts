325bdb1ebf9bdd441fe582c3eeee21349bcb6e5b
Nicolas Vitaterna <nvitaterna@gmail.com>
2024-04-26 20:38:17 -0400

    fix home score

--- src/renderers/score.renderer.ts
+++ src/renderers/score.renderer.ts
@@ -22,8 +22,8 @@ export class ScoreRenderer extends Renderer {
     // home score needs to be 32 - dash width - 1px spacing - home score width
     // away score needs to be 32 + dash width + 1px spacing
 
-    const homeScoreX = 32 - DASH_WIDTH - 1 - scoreFont.stringWidth(homeScore);
-    const awayScoreX = 32 + DASH_WIDTH + 1;
+    const homeScoreX = 32 + DASH_WIDTH + 1;
+    const awayScoreX = 32 - DASH_WIDTH - 1 - scoreFont.stringWidth(homeScore);
 
     this.homeScoreRenderer = new TextRenderer(
       this.matrix,
