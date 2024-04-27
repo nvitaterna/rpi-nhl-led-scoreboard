8bb0fdba6523a9f86f7a9f348493ae4c30a6795f
Nicolas Vitaterna <nvitaterna@gmail.com>
2024-04-26 20:34:45 -0400

    fix

---
+++ src/renderers/clock.renderer.ts
@@ -0,0 +1,78 @@
+import { LedMatrixInstance } from '@nvitaterna/rpi-led-matrix';
+import { Renderer } from '@/renderers/renderer';
+import { UiData } from '@/ui-data/ui-data.schema';
+import { TextRenderer } from '@/renderers/text.renderer';
+import { smallFont } from '@/font/fonts';
+import { PeriodType } from '@/boxscore/boxscore.schema';
+
+const PERIOD_SUFFIXES = ['ST', 'ND', 'RD'];
+const CLOCK_OFFSET = 3;
+
+export class ClockRenderer extends Renderer {
+  private periodRenderer: TextRenderer;
+
+  private timeRenderer: TextRenderer;
+
+  constructor(matrix: LedMatrixInstance, uiData: UiData) {
+    super(matrix);
+
+    const periodText = this.getPeriodText(
+      uiData.boxscore.period ?? 1,
+      uiData.boxscore.periodType ?? 'REGULAR',
+    );
+
+    const timeText = this.getTimeText(
+      uiData.boxscore.minutes ?? 20,
+      uiData.boxscore.seconds ?? 0,
+    );
+
+    const periodCenterX = TextRenderer.getCenteredX(
+      this.matrix,
+      periodText,
+      smallFont,
+    );
+
+    const timeCenterX = TextRenderer.getCenteredX(
+      this.matrix,
+      timeText,
+      smallFont,
+    );
+
+    this.periodRenderer = new TextRenderer(
+      this.matrix,
+      periodCenterX,
+      0 + CLOCK_OFFSET,
+      smallFont,
+      periodText,
+    );
+
+    this.timeRenderer = new TextRenderer(
+      this.matrix,
+      timeCenterX,
+      6 + CLOCK_OFFSET,
+      smallFont,
+      timeText,
+    );
+  }
+
+  private getPeriodText(period: number, periodType: PeriodType) {
+    let periodText = `${period}${PERIOD_SUFFIXES[period - 1]}`;
+
+    if (periodType === 'OVERTIME') {
+      periodText = `OT${period}`;
+    } else if (periodType === 'SHOOTOUT') {
+      periodText = 'SO';
+    }
+
+    return periodText;
+  }
+
+  private getTimeText(minutes: number, seconds: number) {
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  }
+
+  render() {
+    this.periodRenderer.render();
+    this.timeRenderer.render();
+  }
+}
